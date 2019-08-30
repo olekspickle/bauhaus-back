b6ad76fc81579f906b02d15562b2320d28c49582
alekspickle <aleks.work2222@gmail.com>
2019-08-30 14:30:50 +0300

    Updates

---
+++ precache-manifest.a256077e826cb66824e5df09c7a22779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cca360fab0b33de45a138becfafe020",
+    "url": "/bauhaus-back/index.html"
+  },
+  {
+    "revision": "8a8ee5e9eebd7aefffca",
+    "url": "/bauhaus-back/static/css/main.3cc335e0.chunk.css"
+  },
+  {
+    "revision": "00f26363192c96cc18a9",
+    "url": "/bauhaus-back/static/js/2.9e6c1d57.chunk.js"
+  },
+  {
+    "revision": "8a8ee5e9eebd7aefffca",
+    "url": "/bauhaus-back/static/js/main.d5b980eb.chunk.js"
+  },
+  {
+    "revision": "589d10876794838b3664",
+    "url": "/bauhaus-back/static/js/runtime~main.a27f137d.js"
+  }
+]);
